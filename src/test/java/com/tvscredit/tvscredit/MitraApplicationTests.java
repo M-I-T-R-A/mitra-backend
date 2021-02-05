1112b1c7be5e965c9419343438e2cc9de5aae789
dynamic.malay@gmail.com <dynamic.malay@gmail.com>
2021-02-05 23:39:47 +0530

    first commit

---
+++ src/test/java/com/tvscredit/tvscredit/MitraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tvscredit.tvscredit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
