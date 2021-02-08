7354a093fee829d62b9ade6df586618f9eebfec9
dynamic.malay@gmail.com <dynamic.malay@gmail.com>
2021-02-08 17:46:18 +0530

    customer updated

--- src/main/java/com/tvscredit/tvscredit/dto/customer/CustomerBasicDTO.java
+++ src/main/java/com/tvscredit/tvscredit/dto/customer/CustomerBasicDTO.java
@@ -5,6 +5,8 @@ import com.tvscredit.tvscredit.models.enums.Gender;
 
 public class CustomerBasicDTO {
 
+    private Long id;
+
     private String name;
 
     private Gender gender;
@@ -43,6 +45,14 @@ public class CustomerBasicDTO {
 
     public String getName() {
         return name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getAadharCardFrontImageUrl() {
