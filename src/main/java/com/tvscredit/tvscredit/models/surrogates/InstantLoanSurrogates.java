5b9e829443001bd1c7330327bfcb57b9f0086527
dynamic.malay@gmail.com <dynamic.malay@gmail.com>
2021-02-10 16:02:08 +0530

    bills functions updated

--- src/main/java/com/tvscredit/tvscredit/models/surrogates/InstantLoanSurrogates.java
+++ src/main/java/com/tvscredit/tvscredit/models/surrogates/InstantLoanSurrogates.java
@@ -1,13 +1,23 @@
 package com.tvscredit.tvscredit.models.surrogates;
 
 import com.tvscredit.tvscredit.models.person.Customer;
+import com.tvscredit.tvscredit.models.shop.PurchasedItemBill;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.MapsId;
 import javax.persistence.OneToOne;
+import java.util.List;
 
 @Entity
 public class InstantLoanSurrogates extends BaseSurrogates{
+
+    @Override
+    public void setBills(List<PurchasedItemBill> bills) {
+        for (PurchasedItemBill bill:bills){
+            bill.setInstantLoanSurrogates(this);
+        }
+        super.setBills(bills);
+    }
 
 }
