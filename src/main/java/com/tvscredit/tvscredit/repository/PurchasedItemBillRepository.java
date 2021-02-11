06f5b0c191653f6510e9c0789ccf13aabe2cff91
dynamic.malay@gmail.com <dynamic.malay@gmail.com>
2021-02-11 17:18:53 +0530

    Create PurchasedItemBillRepository.java

---
+++ src/main/java/com/tvscredit/tvscredit/repository/PurchasedItemBillRepository.java
@@ -0,0 +1,7 @@
+package com.tvscredit.tvscredit.repository;
+
+import com.tvscredit.tvscredit.models.shop.PurchasedItemBill;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PurchasedItemBillRepository extends CrudRepository<PurchasedItemBill, Long> {
+}
